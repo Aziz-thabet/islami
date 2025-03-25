f1c2fa592e3b4170f7362bdc97f7482338b522ee
hp <azizthabt15@gmail.com>
2025-03-25 18:26:07 +0200

    feat:quran chapters details

--- app/src/main/java/com/example/islami/hom/tabs/quran/QuranFragment.kt
+++ app/src/main/java/com/example/islami/hom/tabs/quran/QuranFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.islami.databinding.FragmentQuranBinding
-import com.example.islami.hom.AppConstens
 import com.example.islami.hom.tabs.chpterDetails.ChapterDetailsActivity
 
 class QuranFragment : Fragment() {
